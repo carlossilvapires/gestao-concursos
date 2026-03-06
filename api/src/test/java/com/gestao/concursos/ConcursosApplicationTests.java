21fa999724fa1bd9868e4cad69e1d8bd46416cff
Carlos Eduardo Silva Pires <carlossilvaptu01@gmail.com>
2026-03-06 20:41:24 -0300

    feat: setup spring boot backend project

---
+++ api/src/test/java/com/gestao/concursos/ConcursosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestao.concursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
